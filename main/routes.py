87b0e12f45d49559be3ccc380cc428edde24c639
zachk <zacharykerr@ufl.edu>
2022-01-29 00:59:45 -0500

    bootstrap setup

--- main/routes.py
+++ main/routes.py
@@ -2,7 +2,12 @@ from flask import render_template, request, Blueprint
 
 main = Blueprint('main', __name__)
 
+
 @main.route("/")
 @main.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template('layout.html')
+
+@main.route("/about")
+def about():
+    return render_template('about.html')
